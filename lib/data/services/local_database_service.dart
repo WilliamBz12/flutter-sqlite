ce0ba0efd0a8db4ea164cf97d3c7221aa2a0390f
Williambz12 <williamsilva754@gmail.com>
2024-12-15 21:04:33 -0300

    video 2.1

--- lib/data/services/local_database_service.dart
+++ lib/data/services/local_database_service.dart
@@ -12,7 +12,16 @@ class LocalDatabaseService {
       path,
       version: 1,
       onCreate: (db, version) {
-        print('Banco de dados criado no caminho: $path');
+        db.execute('''
+          CREATE TABLE tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT,
+            isCompleted INTEGER DEFAULT 0,
+            category TEXT
+          )
+        ''');
+        print('Tabela criada: tasks');
       },
     );
   }
