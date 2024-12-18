1a0d54bb2e8db9c33a6f052a5d84398ab552809b
Williambz12 <williamsilva754@gmail.com>
2024-12-18 18:45:27 -0300

    video 2.2

--- lib/domain/models/task/task.dart
+++ lib/domain/models/task/task.dart
@@ -12,4 +12,23 @@ class Task {
     required this.category,
     required this.isCompleted,
   });
+
+  factory Task.fromMap(Map<String, dynamic> data) {
+    return Task(
+      title: data['title'],
+      description: data['description'],
+      category: data['category'],
+      id: data['id'],
+      isCompleted: data['isCompleted'] == 1,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'description': description,
+      'category': category,
+      'isCompleted': isCompleted ? 1 : 0,
+    };
+  }
 }
