7e286e8379d2a5b084f17002f1c9ceabf8636af5
Williambz12 <williamsilva754@gmail.com>
2024-12-18 18:18:12 -0300

    video 1.3

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -2,6 +2,7 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:sqlite_offline/data/repositories/mock_task_repository.dart';
 import 'package:sqlite_offline/data/repositories/task_repository.dart';
+import 'package:sqlite_offline/data/services/local_database_service.dart';
 import 'package:sqlite_offline/domain/use_cases/task/add_task_use_case.dart';
 import 'package:sqlite_offline/domain/use_cases/task/delete_task_use_case.dart';
 import 'package:sqlite_offline/domain/use_cases/task/get_task_use_case.dart';
@@ -37,6 +38,9 @@ List<SingleChildWidget> get providersLocal {
         updateTaskUseCase: context.read(),
         deleteTaskUseCase: context.read(),
       ),
-    )
+    ),
+    Provider<LocalDatabaseService>(
+      create: (context) => LocalDatabaseService(),
+    ),
   ];
 }
--- lib/data/services/local_database_service.dart
+++ lib/data/services/local_database_service.dart
@@ -1,5 +1,20 @@
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class LocalDatabaseService {
   static Database? _database;
+
+  Future<void> init() async {
+    final path = await getDatabasesPath();
+    final dbPath = join(path, 'tasks.db');
+
+    _database = await openDatabase(
+      dbPath,
+      version: 1,
+      onCreate: (db, version) {
+        debugPrint("Banco de dados criado!");
+      },
+    );
+  }
 }
