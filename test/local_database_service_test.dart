38f4cb1561a9a0a62f87b93b4ab8b24e6e46e903
Williambz12 <williamsilva754@gmail.com>
2025-01-20 08:56:58 -0300

    video 5.3

--- test/local_database_service_test.dart
+++ test/local_database_service_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqlite_offline/data/services/local_database_service.dart';
+import 'package:sqlite_offline/domain/models/task/task.dart';
 
 void main() {
   late final LocalDatabaseService localDatabaseService;
-  setUp(
+  setUpAll(
     () async {
       sqfliteFfiInit();
       databaseFactory = databaseFactoryFfi;
@@ -18,6 +19,21 @@ void main() {
     'Banco de dados deve inicializar corretamente',
     () {
       expect(localDatabaseService.database?.isOpen, equals(true));
+    },
+  );
+
+  test(
+    'Deverá criar uma task na tabela',
+    () async {
+      final task = Task(
+        title: 'Fazer dever de casa',
+        description: '',
+        category: 'atividades',
+        isCompleted: false,
+        priority: 'alta',
+      );
+      final result = await localDatabaseService.createTask(task);
+      expect(result, isNot(equals(null)));
     },
   );
 }
